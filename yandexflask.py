192867001d621d8e419a7b1956d4c5c78abb548e
Dmitry315 <vgurskoy@mail.ru>
2019-02-14 16:39:24 +0300

    'add'

--- yandexflask.py
+++ yandexflask.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
+@app.route('/index')
+def index():
+    return 'Привет, Яндекс! Я - Дмитрий'
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+@app.route('/image_sample')
+def image():
+    return '''
+    <h1>Картинка<h1>
+    <img src="{}">
+    '''.format(url_for('static', filename='img/my_img.jpg'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080, host='127.0.0.1')
