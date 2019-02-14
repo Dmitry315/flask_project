10037d6c973f10db8978096eee35c2eab85fdd17
Dmitry315 <vgurskoy@mail.ru>
2019-02-14 17:48:14 +0300

    fix music

--- yandexflask.py
+++ yandexflask.py
@@ -103,7 +103,7 @@ def music(track):
 
                   </head>
                   <body>
-                    <frame src="https://music.yandex.ru/album/4766/track/57703">
+                    <iframe frameborder="0" style="border:none;width:600px;height:100px;" width="600" height="100" src="https://music.yandex.ru/iframe/#track/57703/4766/">Слушайте <a href='https://music.yandex.ru/album/4766/track/57703'>Enter Sandman</a> — <a href='https://music.yandex.ru/artist/3121'>Metallica</a> на Яндекс.Музыке</iframe>
 
                     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
