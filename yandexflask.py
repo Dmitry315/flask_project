e514b9f36b61e4c0ac89d620b582f8d2d60e1aec
Dmitry315 <vgurskoy@mail.ru>
2019-02-14 16:51:27 +0300

    add tag

--- yandexflask.py
+++ yandexflask.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route('/index')
 def index():
-    return 'Привет, Яндекс! Я - Дмитрий'
+    return '<h1>Привет, Яндекс! Я - Дмитрий</h1>'
 
 @app.route('/image_sample')
 def image():
     return '''
     <h1>Картинка<h1>
-    <img src="{}">
-    '''.format(url_for('static', filename='img/my_img.jpg'))
+    <img src="'''+url_for('static', filename='img/my_img.jpg')+'''">
+    '''
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
