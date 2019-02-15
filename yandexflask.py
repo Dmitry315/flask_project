9559de97784f8971e3b4346345c97879a2b2425b
Dmitry315 <vgurskoy@mail.ru>
2019-02-15 16:47:41 +0300

    task video

--- yandexflask.py
+++ yandexflask.py
@@ -62,5 +62,40 @@ def table(n, m):
                     </html>
         '''
 
+@app.route('/youtube/<int:n>')
+def video(n):
+    links = ['<iframe width="560" height="315" src="https://www.youtube.com/embed/tAGnKpE4NCI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+             '<iframe width="560" height="315" src="https://www.youtube.com/embed/CD-E-LDc384" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+             '<iframe width="560" height="315" src="https://www.youtube.com/embed/c8qrwON1-zE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+             '<iframe width="560" height="315" src="https://www.youtube.com/embed/eeqGuaAl6Ic" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+             '<iframe width="560" height="315" src="https://www.youtube.com/embed/UO_84C3fpuI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']
+    if n < 1 or n > 5:
+        return '''ERROR: wrong url, last number should be: 1, 2, 3, 4 or 5'''
+    return '''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width,
+                            initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                            crossorigin="anonymous">
+
+                          </head>
+                          <body>
+                            '''+'<br>'.join([links[i] for i in range(n)])+'''
+
+                            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+                            <script>
+
+                            </script>
+                          </body>
+                        </html>
+            '''
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
