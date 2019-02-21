4b89dd069bf98042eeeb0181f559ffb291e23ecc
Dmitry315 <vgurskoy@mail.ru>
2019-02-21 16:13:48 +0300

    add users model

--- models.py
+++ models.py
@@ -12,3 +12,37 @@ class DB:
     def __del__(self):
         self.conn.close()
 
+
+class UsersModel:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def init_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users 
+                                (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                 user_name VARCHAR(50),
+                                 password_hash VARCHAR(128)
+                                 )''')
+        cursor.close()
+        self.connection.commit()
+
+    def exists(self, user_name):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE user_name = ?",
+                       (user_name,))
+        row = cursor.fetchone()
+        return (True, row[0]) if row else (False,)
+
+    def get(self, id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE id = ?", (str(id)))
+        row = cursor.fetchone()
+        return row
+
+    def get_all(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users", (str(id)))
+        row = cursor.fetchall()
+        return row
+
